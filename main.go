eb707245c26ae1f9c23a9596c0567fffc0853d13
ImUndeniable <tarun282000@gmail.com>
2026-01-07 14:14:26 +0530

    Context and OS Package has been implemented

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
@@ -14,14 +17,25 @@ type Job struct {
 type Result struct {
 	URL    string
 	Status int
+	Error  error
 }
 
 func worker(id int, jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := range jobs {
+		//Context - 2 secs timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel() // always cancel the free resources
+		//create the request with context
+		req, err := http.NewRequestWithContext(ctx, "GET", i.URL, nil)
+		if err != nil {
+			results <- Result{URL: i.URL, Status: 0, Error: err}
+			continue
+		}
+
 		fmt.Printf("Worker %d: Checking %s\n", id, i.URL)
-		// Actual HTTP CALL
-		resp, err := http.Get(i.URL)
+		// Execute the request
+		resp, err := http.DefaultClient.Do(req)
 
 		status := 0
 		if err == nil {
@@ -32,6 +46,7 @@ func worker(id int, jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) 
 		results <- Result{
 			URL:    i.URL,
 			Status: status,
+			Error:  err,
 		}
 		/*final := Result{
 			URL:    i.URL,
@@ -45,26 +60,31 @@ func main() {
 	start := time.Now()
 
 	var wg sync.WaitGroup
-	jobs := make(chan Job, 5)
-	results := make(chan Result, 5)
+	jobs := make(chan Job, 35)
+	results := make(chan Result, 35)
 
 	//workers creation
-	for w := 1; w <= 3; w++ {
+	for w := 1; w <= 10; w++ {
 		wg.Add(1)
 		go worker(w, jobs, results, &wg)
 	}
 
-	//URLS
-	urls := []string{"https://www.youtube.com", "https://www.google.com", "https://www.linkedin.com", "https://www.naukri.com"}
-
-	// jobs creation
-	for i := range urls {
-		url := Job{
-			URL: urls[i],
-		}
-		jobs <- url
+	file, err := os.Open("websites.txt")
+	if err != nil {
+		fmt.Println("The file is not readable/avaiable")
+		return
 	}
-	close(jobs)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// jobs creation
+	go func() {
+		for scanner.Scan() {
+			url := scanner.Text()
+			jobs <- Job{URL: url}
+		}
+		close(jobs)
+	}()
 
 	go func() {
 		wg.Wait()
@@ -73,6 +93,10 @@ func main() {
 
 	//captures result
 	for res := range results {
+		if res.Error != nil {
+			fmt.Printf("Result: %s ->Status: ERROR (%v)\n", res.URL, res.Error)
+			continue
+		}
 		fmt.Printf("Result: %s -> Status: %d\n", res.URL, res.Status)
 	}
 
